18076a0dcc987bf2715253e47db89c5d02aee2c5
Fatima Ejaz <92662988+fatima2k18@users.noreply.github.com>
2025-06-23 07:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
 inputs[0].focus(); // Set initial focus
@@ -6,7 +5,6 @@ inputs[0].focus(); // Set initial focus
 inputs.forEach((input, index) => {
   input.addEventListener("input", (e) => {
     const value = e.target.value;
-
     if (value) {
       if (index < inputs.length - 1) {
         inputs[index + 1].focus();
@@ -16,13 +14,16 @@ inputs.forEach((input, index) => {
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (e.target.value === "") {
-        if (index > 0) {
-          inputs[index - 1].focus();
-          inputs[index - 1].value = ""; // Clear previous field on backspace
-        }
-      } else {
-        e.target.value = ""; // Clear current field
+      e.preventDefault();
+
+      // Clear current value
+      inputs[index].value = "";
+
+      // Move focus to previous
+      if (index > 0) {
+        inputs[index - 1].focus();
+        // Optional: also clear the previous field if it's filled
+        // inputs[index - 1].value = "";
       }
     }
   });
@@ -30,13 +31,11 @@ inputs.forEach((input, index) => {
   input.addEventListener("paste", (e) => {
     e.preventDefault();
     const pasteData = e.clipboardData.getData("text").replace(/\D/g, "");
-
     pasteData.split("").forEach((char, i) => {
       if (i < inputs.length) {
         inputs[i].value = char;
       }
     });
-
     const filledIndex = Math.min(pasteData.length, inputs.length - 1);
     inputs[filledIndex].focus();
   });
